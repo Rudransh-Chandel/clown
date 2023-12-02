1b6f4e4d74e4398b57cfbaa4c0b7e1c4225a110f
Rudransh-Chandel <106671046+Rudransh-Chandel@users.noreply.github.com>
2023-12-02 17:31:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ function setup()
 {
     canvas = createCanvas(300,300);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    pose_net= ml5.poseNet(video,modelLoaded);
+    pose_net.on("pose",gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Pose Net is Working");
 }
 
 function draw()
